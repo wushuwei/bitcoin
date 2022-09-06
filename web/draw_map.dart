2d622844b908a64d1aaf698334f9b6308478fcc5
Shu Wei Wu <wushuwei@gmail.com>
2022-09-06 00:13:22 -0400

    v1.3 flame draw canvas

--- web/draw_map.dart
+++ web/draw_map.dart
@@ -5,42 +5,52 @@ import 'package:flame/game.dart';
 
 import 'package:flutter/widgets.dart';
 // import 'dart:ui';
+import 'dart:async';
+import 'dart:ui' as ui;
+import 'package:flame/cache.dart';
+final imagesLoader = Images();
 
-class MyCrate extends SpriteComponent {
-  MyCrate() : super(size: Vector2.all(160));
-  // MyCrate() : super();
-
-  @override
-  Future<void> onLoad() async {
-    sprite = await Sprite.load('two.png'
-      ,srcPosition: Vector2(0, 0)
-      ,srcSize: Vector2(160.0, 160.0)
-        );
-
-  }
-
-}
 
 class MapAsGame extends FlameGame {
   late Size screenSize;
+  late double x = 0.0;
+  late double y = 0.0;
+
 
   @override
-  Future<void> onLoad() async {
-    await add(MyCrate());
+  Future<Function> onLoad() {
+
   }
 
-  Future<void> drawImage() async {
-    final image = await images.load('two.png');
-    final playerFrame = Sprite(
+  @override
+  void render(Canvas canvas) {
+    // drawRect(canvas);
+
+    drawImage(canvas);
+  }
+
+
+  @override
+  void update(double dt) {
+    x = (x + 1.0) % 320;
+    y = (y + 1.0) % 320;
+
+  }
+
+
+
+  void drawImage(Canvas canvas) async {
+    final image = await images.load('player.png');
+    final sprite = Sprite(
       image,
-      srcPosition: Vector2(132.0, 120.0),
-      srcSize: Vector2(160.0, 160.0),
+      srcPosition: Vector2(x, y),
+      srcSize: Vector2(16.0, 16.0),
     );
 
   }
 
   void drawRect(Canvas canvas) {
-    Rect bgRect = Rect.fromLTWH(0, 0, 20, 20);
+    Rect bgRect = Rect.fromLTWH(x, y, 20, 20);
     Paint bgPaint = Paint();
     bgPaint.color = Color(0xff576574);
     canvas.drawRect(bgRect, bgPaint);
