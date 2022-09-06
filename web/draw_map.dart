a8fab6b742430d5533c05bddff5eb50fb4f9e2b9
Shu Wei Wu <wushuwei@gmail.com>
2022-09-05 23:19:40 -0400

    v1.3 flame draw canvas

--- web/draw_map.dart
+++ web/draw_map.dart
@@ -1,27 +1,60 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
-import 'package:flame/flame.dart';
-import 'package:flutter/widgets.dart';
-import 'dart:ui';
+// import 'package:flame/flame.dart';
+// import 'package:flame/sprite.dart';
 
-class MapAsGame extends Game {
+import 'package:flutter/widgets.dart';
+// import 'dart:ui';
+
+class MyCrate extends SpriteComponent {
+  MyCrate() : super(size: Vector2.all(160));
+  // MyCrate() : super();
+
+  @override
+  Future<void> onLoad() async {
+    sprite = await Sprite.load('two.png'
+      ,srcPosition: Vector2(0, 0)
+      ,srcSize: Vector2(160.0, 160.0)
+        );
+
+  }
+
+}
+
+class MapAsGame extends FlameGame {
   late Size screenSize;
 
   @override
-  void render(Canvas canvas) {
+  Future<void> onLoad() async {
+    await add(MyCrate());
+  }
+
+  Future<void> drawImage() async {
+    final image = await images.load('two.png');
+    final playerFrame = Sprite(
+      image,
+      srcPosition: Vector2(132.0, 120.0),
+      srcSize: Vector2(160.0, 160.0),
+    );
+
+  }
+
+  void drawRect(Canvas canvas) {
     Rect bgRect = Rect.fromLTWH(0, 0, 20, 20);
     Paint bgPaint = Paint();
     bgPaint.color = Color(0xff576574);
     canvas.drawRect(bgRect, bgPaint);
   }
 
-  @override
-  void update(double t) {}
-
-
 
 }
-void main() {
-  final game = MapAsGame();
 
-  runApp(GameWidget(game: game));
+void main() {
+  final myGame = MapAsGame();
+  runApp(
+    GameWidget(
+      game: myGame,
+    ),
+  );
+
 }
