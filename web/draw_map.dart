3b95c2ff061f7d2f0f112bb5b8ed47528374a4dc
Shu Wei Wu <wushuwei@gmail.com>
2022-09-06 10:27:58 -0400

    v1.4 flame draw two person on canvas

--- web/draw_map.dart
+++ web/draw_map.dart
@@ -8,21 +8,24 @@ import 'package:flutter/widgets.dart';
 import 'dart:async';
 import 'dart:ui' as ui;
 import 'package:flame/cache.dart';
-final imagesLoader = Images();
 
+final imagesLoader = Images();
 
 class MapAsGame extends FlameGame {
   late Size screenSize;
   late double x = 0.0;
   late double y = 0.0;
 
-  late Sprite block;
+  late double x2 = 0.0;
+  late double y2 = 0.0;
+
+  late Sprite metTogether;
+  late Sprite onePerson;
 
   @override
   Future<void> onLoad() async {
-    // other loads omitted
-    final image = await images.load('two.png');
-    block = Sprite(image);
+    metTogether = await preLoadImage('two.png');
+    onePerson = await preLoadImage('one.jpg');
   }
 
   @override
@@ -30,30 +33,25 @@ class MapAsGame extends FlameGame {
     // drawRect(canvas);
     // drawImage(canvas);
 
-    block.render(canvas
-        ,position: Vector2(x, y)
-        ,size: Vector2(16.0, 16.0)
-    );
-  }
+    metTogether.render(canvas,
+        position: Vector2(x, y), size: Vector2(16.0, 16.0));
 
+    onePerson.render(canvas,
+        position: Vector2(x2, y2), size: Vector2(32.0, 32.0));
+  }
 
   @override
   void update(double dt) {
     x = (x + 1.0) % 320;
     y = (y + 1.0) % 320;
 
+    x2 = (x2 + 2.0) % 320;
+    y2 = (y2 + 4.0) % 320;
   }
 
-
-
-  void drawImage(Canvas canvas) async {
-    final image = await images.load('player.png');
-    final sprite = Sprite(
-      image,
-      srcPosition: Vector2(x, y),
-      srcSize: Vector2(16.0, 16.0),
-    );
-
+  Future<Sprite> preLoadImage(String imageName) async {
+    final image = await images.load(imageName);
+    return Sprite(image);
   }
 
   void drawRect(Canvas canvas) {
@@ -62,8 +60,6 @@ class MapAsGame extends FlameGame {
     bgPaint.color = Color(0xff576574);
     canvas.drawRect(bgRect, bgPaint);
   }
-
-
 }
 
 void main() {
@@ -73,5 +69,4 @@ void main() {
       game: myGame,
     ),
   );
-
 }
